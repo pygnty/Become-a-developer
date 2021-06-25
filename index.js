17996c8b5f59fea95b855b1757c8b6f10bff5b9b
pygnty <47454562+pygnty@users.noreply.github.com>
2021-06-25 21:28:53 +0300

    Переделывание
    Псевдо-прогеры устроили взаимный код-ревью

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const getUserNames = (data) => {
   let x;
   let set = new Set();
   for(let i = 0; i < data.length; i++) {
-    x = data.indexOf('user: ', i);
+    x = data.indexOf('user: ', i); // слишком много индексофов. Через чур. Я понимаю, что ты делала без встроенных, но индексоф - уже сама по себе встроенная функция
     if(x === -1) break;
     i = data.indexOf(',', x);
     set.add(data.slice(x+6, i));
@@ -20,13 +20,15 @@ const getUserNames = (data) => {
 
 const compareFiles = (file1, file2) => {
   const file1Content = readFile(file1);
-  const file2Content = readFile(file2);
   const userNames1 = [...getUserNames(file1Content)];
+  delete file1Content; //использовал - удалил.
+  const file2Content = readFile(file2);
   const userNames2 = [...getUserNames(file2Content)];
-  let resultStr = "";
+  delete file1Content; // использовал - удалил
+  let resultStr = ""; 
   for(let i = 0; i < userNames1.length; i++) {
-    if(userNames2.indexOf(userNames1[i]) === -1) {
-      resultStr += userNames1[i] + "\n";
+    if(userNames2.indexOf(userNames1[i]) === -1) { // Оператор indexOf(userNames1[i]) крайне медленный. По фатку, ты сканишь массивы в среднем n*log(m) раз. А это дофига
+      resultStr += userNames1[i] + "\n";           // Было бы логичнее оставить результаты в сетах и просто сравнивать if( userNames2[userNames1[i]] )
     }
   }
   
